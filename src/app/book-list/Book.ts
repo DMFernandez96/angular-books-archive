6259e47bda6580bbb1937b967daafd0748cce7a9
Manuela Fernandez <manu.fer.0396@gmail.com>
2020-10-13 00:16:25 -0300

    agrega interfaz Book y template del componente book-list

---
+++ src/app/book-list/Book.ts
@@ -0,0 +1,10 @@
+import { Tracing } from "trace_events";
+
+export interface Book{
+    titulo: string;
+    autor: string;
+    genero: string;
+    valoracion: number;
+    imagen: string; //pq es una url
+    nytimes: boolean; //estuvo en la lista del new york times?
+}
--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from './Book';
 
 @Component({
   selector: 'app-book-list',
@@ -7,13 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookListComponent implements OnInit {
 
-  book = {
-    "titulo": "Tokio Blues",
-    "autor": "Haruki Murakami",
-    "genero": "Realismo",
-    "valoracion": 4.5,
-    "imagen": "assets/img/norwegianwood.jpg"
-  }
+  books: Book[] = [
+    {
+      titulo: "Tokio Blues",
+      autor: "Haruki Murakami",
+      genero: "Realismo",
+      valoracion: 4.5,
+      imagen: "assets/img/norwegianwood.jpg",
+      nytimes: false,
+    }, 
+    {
+      titulo: "Los juegos del hambre",
+      autor: "Sussan Collins",
+      genero: "Distopia",
+      valoracion: 4,
+      imagen: "assets/img/norwegianwood.jpg",
+      nytimes: true,
+    },
+    {
+      titulo: "La odisea",
+      autor: "Homero",
+      genero: "Poesia epica griega",
+      valoracion: 0,
+      imagen: "assets/img/norwegianwood.jpg",
+      nytimes: false,
+    },
+  ];
 
   constructor() { }
 
